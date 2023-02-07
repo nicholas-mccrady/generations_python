103bb15ef2aa1a7fdc9a999f5761d6466784c2b5
nicholas-mccrady <nicholas.mccrady@gmail.com>
2023-02-06 19:14:40 -0600

    added new project file and finished while_loop_scott

--- while_loop_scott.py
+++ while_loop_scott.py
@@ -11,7 +11,7 @@ roll_number = 0
 the_count = 0
 the_prev_number = 0
 # made a varible to catch the previous number
-while the_count < students_inf:
+while the_count < students_inf and roll_number < 200000:
     the_number = randint(1,9)
     if the_number == the_prev_number:
         the_count = the_count + 1
@@ -19,8 +19,10 @@ while the_count < students_inf:
 
     else: 
         the_count = 0
-        print(str(roll_number) + " " + "the numbers do not match" + " "  + str(the_number) + " <> " + str(the_prev_number))
+        print(str(roll_number) + " " + "the numbers do not match" + " "  + str(the_number) + " != " + str(the_prev_number))
 
     the_prev_number = the_number
     roll_number = roll_number + 1
+    if roll_number == 200000:
+        print("dont play the lottery")
 input("press enter")
---
+++ while_loop_user_driven_scott.py
@@ -0,0 +1,17 @@
+### a user driven while loop
+
+user_choice = "y"
+
+
+
+while user_choice == "y":
+    print("welcome to my loop")
+    user_choice = input("would you like to continue?: (y/n?) ")
+
+
+
+
+
+
+
+input("press enter...")
