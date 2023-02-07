7d737c4ebcd79a6d451de956cd06bb1c5fd3f489
nicholas-mccrady <nicholas.mccrady@gmail.com>
2023-02-06 18:55:31 -0600

    adding days exercises

---
+++ while_loop_scott.py
@@ -0,0 +1,26 @@
+### we have a program to demonstrate the while loop
+
+
+# import the random module
+from random import randint
+
+students_inf = int(input("how many numbers in a row would you like to hit ?: "))
+students_other = randint(1,10) 
+
+roll_number = 0
+the_count = 0
+the_prev_number = 0
+# made a varible to catch the previous number
+while the_count < students_inf:
+    the_number = randint(1,9)
+    if the_number == the_prev_number:
+        the_count = the_count + 1
+        print(str(roll_number) + " the numbers match" + " "  + str(the_number) + " = " + str(the_prev_number))
+
+    else: 
+        the_count = 0
+        print(str(roll_number) + " " + "the numbers do not match" + " "  + str(the_number) + " <> " + str(the_prev_number))
+
+    the_prev_number = the_number
+    roll_number = roll_number + 1
+input("press enter")
